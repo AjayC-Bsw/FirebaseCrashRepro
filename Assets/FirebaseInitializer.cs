882c36e59151789ceb1c58206ca9a5cd22287a47
AjayC-Bsw <ajay@bswgames.com>
2021-11-11 23:04:40 +0530

    first commit

---
+++ Assets/FirebaseInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+// using Firebase.Messaging;
+// using GoogleMobileAds.Api;
+public class FirebaseInitializer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(_ => Debug.Log("firebase initialized"));
+
+        
+    }
+
+
+}
